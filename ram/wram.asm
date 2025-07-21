cb4fff4be4ed406304fc83f15790855ad3b45d89
CreamElDudJafar <jahern2398@gmail.com>
2025-07-21 17:41:25 -0500

    Can Fly To Route 4 and Route 10 Pokemon Centers Now
    https://github.com/pret/pokered/wiki/Allow-to-fly-to-any-map-and-from-any-map

--- ram/wram.asm
+++ ram/wram.asm
@@ -741,7 +741,7 @@ wRivalStarterBallSpriteIndex:: db
 NEXTU
 wFlyAnimUsingCoordList:: db
 ; $ff sentinel values at each end
-wFlyLocationsList:: ds NUM_CITY_MAPS + 2
+wFlyLocationsList:: ds NUM_CITY_MAPS + 4
 
 NEXTU
 wWhichTownMapLocation:: db
@@ -2177,7 +2177,7 @@ wWalkBikeSurfState:: db
 
 	ds 10
 
-wTownVisitedFlag:: flag_array NUM_CITY_MAPS
+wTownVisitedFlag:: flag_array NUM_CITY_MAPS + 2
 
 ; starts at 502
 wSafariSteps:: dw
