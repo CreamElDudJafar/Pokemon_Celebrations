38b88f322e21139af9389a1709cc530abe95d65c
CreamElDudJafar <jahern2398@gmail.com>
2025-07-31 21:05:16 -0500

    Fix Printer Sound Bug
    Right Now Placed Printer Stuff in Audio Engine 3 to prevent game from crashing

--- audio/headers/musicheaders3.asm
+++ audio/headers/musicheaders3.asm
@@ -114,3 +114,8 @@ Music_MeetMaleTrainer::
 	channel 1, Music_MeetMaleTrainer_Ch1
 	channel 2, Music_MeetMaleTrainer_Ch2
 	channel 3, Music_MeetMaleTrainer_Ch3
+
+Music_GBPrinter::
+	channel_count 1
+	channel 3, Music_GBPrinter_Ch3
+
--- constants/music_constants.asm
+++ constants/music_constants.asm
@@ -55,8 +55,6 @@ ENDM
 	music_const MUSIC_MEET_EVIL_TRAINER,   Music_MeetEvilTrainer
 	music_const MUSIC_MEET_FEMALE_TRAINER, Music_MeetFemaleTrainer
 	music_const MUSIC_MEET_MALE_TRAINER,   Music_MeetMaleTrainer
-
-	; AUDIO_4
 	music_const MUSIC_GB_PRINTER,          Music_GBPrinter
 
 	; AUDIO_1 AUDIO_2 AUDIO_3
