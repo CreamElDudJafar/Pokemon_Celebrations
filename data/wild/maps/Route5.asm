5bab8ac78479e3ae7a13101b047d7373ba6deeb4
CreamElDudJafar <jahern2398@gmail.com>
2025-11-13 10:51:44 -0600

    Update Wild Mon Locations

--- data/wild/maps/Route5.asm
+++ data/wild/maps/Route5.asm
@@ -3,7 +3,7 @@ Route5WildMons:
 IF (DEF(_RED) || DEF(_GREEN)) 
 	db 13, ODDISH
 	db 13, PIDGEY
-	db 15, PIDGEY
+	db 15, FARFETCHD
 	db 10, MEOWTH
 	db 12, MANKEY
 	db 15, ODDISH
@@ -15,7 +15,7 @@ ENDC
 IF DEF(_BLUE)
 	db 13, BELLSPROUT
 	db 13, PIDGEY
-	db 15, PIDGEY
+	db 15, FARFETCHD
 	db 10, MEOWTH
 	db 12, MANKEY
 	db 15, BELLSPROUT
