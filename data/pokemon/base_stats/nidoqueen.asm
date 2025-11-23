19a7869c44f9c5c387c040d300dbb7186894d502
CreamElDudJafar <jahern2398@gmail.com>
2025-11-22 19:50:03 -0600

    Add TM51 Flamethrower
    Also restored Whirlwind TM ability to Pokemon. Also changed Pokemon Stats.

--- data/pokemon/base_stats/nidoqueen.asm
+++ data/pokemon/base_stats/nidoqueen.asm
@@ -10,17 +10,17 @@
 	INCBIN "gfx/pokemon/front/nidoqueen.pic", 0, 1 ; sprite dimensions
 	dw NidoqueenPicFront, NidoqueenPicBack
 
-	db TACKLE, SCRATCH, TAIL_WHIP, BODY_SLAM ; level 1 learnset
+	db TACKLE, SCRATCH, DIG, BODY_SLAM ; level 1 learnset
 	db GROWTH_MEDIUM_SLOW ; growth rate
 
 	; tm/hm learnset
-	tmhm MEGA_PUNCH,   MEGA_KICK,    TOXIC,        HORN_DRILL,   BODY_SLAM,    \
+	tmhm MEGA_PUNCH,  MEGA_KICK, TOXIC, HORN_DRILL, BODY_SLAM,   REST, \
 	     TAKE_DOWN,    DOUBLE_EDGE,  BUBBLEBEAM,   WATER_GUN,    ICE_BEAM,     \
 	     BLIZZARD,     HYPER_BEAM,   PAY_DAY,      SUBMISSION,   COUNTER,      \
 	     SEISMIC_TOSS, RAGE,         THUNDERBOLT,  THUNDER,      EARTHQUAKE,   \
-	     FISSURE,      MIMIC,        DIG,          DOUBLE_TEAM,  REFLECT,      \
-	     BIDE,         FIRE_BLAST,   SKULL_BASH,   REST,         ROCK_SLIDE,   \
-	     SUBSTITUTE,   SURF,         STRENGTH
+	     FISSURE,      MIMIC,        DIG,          DOUBLE_TEAM,  REFLECT,       \
+	     BIDE,        FIRE_BLAST,   SKULL_BASH,   ROCK_SLIDE,   SUBSTITUTE,    \
+	     FLAMETHROWER, SURF,        STRENGTH
 	; end
 
 	db BANK(NidoqueenPicFront)
