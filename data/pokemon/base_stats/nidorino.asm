fe3a28afa193c129579df3f099c148a46588e11d
CreamElDudJafar <jahern2398@gmail.com>
2025-11-22 19:50:03 -0600

    Add TM51 Flamethrower
    Also restored Whirlwind TM ability to Pokemon. Also changed Pokemon Stats.

--- data/pokemon/base_stats/nidorino.asm
+++ data/pokemon/base_stats/nidorino.asm
@@ -10,14 +10,14 @@
 	INCBIN "gfx/pokemon/front/nidorino.pic", 0, 1 ; sprite dimensions
 	dw NidorinoPicFront, NidorinoPicBack
 
-	db LEER, TACKLE, HORN_ATTACK, NO_MOVE ; level 1 learnset
+	db LEER, TACKLE, NO_MOVE, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_SLOW ; growth rate
 
 	; tm/hm learnset
 	tmhm TOXIC,        HORN_DRILL,   BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  \
 	     BUBBLEBEAM,   WATER_GUN,    ICE_BEAM,     BLIZZARD,     RAGE,         \
-	     THUNDERBOLT,  THUNDER,      MIMIC,        DIG,         DOUBLE_TEAM,   \
-	     REFLECT,       BIDE,         SKULL_BASH,   REST,         SUBSTITUTE
+	     THUNDERBOLT,  THUNDER,      DIG,          MIMIC,        DOUBLE_TEAM,  \
+	     REFLECT,      BIDE,         SKULL_BASH,   REST,         SUBSTITUTE
 	; end
 
 	db BANK(NidorinoPicFront)
