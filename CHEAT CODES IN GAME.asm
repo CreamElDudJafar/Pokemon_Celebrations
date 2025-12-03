459c229b1a92c1d7c894eda51bff63ff7473d278
CreamElDudJafar <jahern2398@gmail.com>
2025-12-02 22:08:25 -0600

    Update Cheat File Codes

---
+++ CHEAT CODES IN GAME.asm
@@ -0,0 +1,27 @@
+CHEATS:
+
+THE FOLLOWING CHEATS WILL WORK IN GAME: DISABLE ONCE CHEAT IS DONE BEING USED...... USE AT OWN RISK. MAY CAUSE OTHER BUGS/GLITCHES.
+
+
+Infinite Money:
+
+019948D3
+019949D3
+01994AD3 ; After done disable it. Noticed weird bugs in graphics when enabled
+
+Walk Through Wall:
+
+010138CD
+
+Rare Candies in Mart:
+
+0128E3D0
+
+Master Balls in Mart:
+
+0101E3D0
+
+9900 Coins:
+
+0199A5D5
+0199A5D5
