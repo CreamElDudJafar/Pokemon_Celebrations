51fa73e3321d4fd934f9671e8bbfe78d8fef4327
CreamElDudJafar <jahern2398@gmail.com>
2025-12-02 20:24:37 -0600

    Level Up Moves Fixed
    Thanks: ShinPokeRed, PureRGB

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -7063,6 +7063,10 @@ DetermineWildOpponent:
 	callfar TryDoWildEncounter
 	ret nz
 InitBattleCommon:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; shinpokerednote: ADDED: store PKMN Levels at the beginning of the Battle.
+	farcall StorePKMNLevels
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	ld a, [wMapPalOffset]
 	push af
 	ld hl, wLetterPrintingDelayFlags
