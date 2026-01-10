f01bdb9ccf44b563926115c621a8a5ee574effc9
CreamElDudJafar <jahern2398@gmail.com>
2026-01-10 07:55:29 -0600

    Update HM Overworld Flash
    Hopefully for final time

--- home/overworld.asm
+++ home/overworld.asm
@@ -110,7 +110,6 @@ OverworldLoopLessDelay::
 
 	; Check for field moves that interact with the bg.
 	predef TryFieldMove
-	callfar TryFlash ; jp OverworldLoop if succeeds
 	jp OverworldLoop
 
 .displayDialogue
