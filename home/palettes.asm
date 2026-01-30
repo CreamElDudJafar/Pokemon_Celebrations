3e25825e7edb0dca7c4c9faea8a889b133bdc87f
CreamElDudJafar <jahern2398@gmail.com>
2026-01-30 12:27:22 -0600

    Place CGB/GBC Palettes into own file
    like how pokeyellow does it. To keep it separated/simplified.

--- home/palettes.asm
+++ home/palettes.asm
@@ -17,75 +17,6 @@ Delay3::
 	ld c, 3
 	jp DelayFrames
 
-;gbcnote - new functions
-UpdateGBCPal_BGP::
-	push af
-	ld a, [hGBC]
-	and a
-	jr z, .notGBC
-	push bc
-	push de
-	push hl
-	ld a, [rBGP]
-	ld b, a
-	ld a, [wLastBGP]
-	cp b
-	jr z, .noChangeInBGP
-	farcall _UpdateGBCPal_BGP
-.noChangeInBGP
-	pop hl
-	pop de
-	pop bc
-.notGBC
-	pop af
-	ret
-	
-UpdateGBCPal_OBP0::
-	push af
-	ld a, [hGBC]
-	and a
-	jr z, .notGBC
-	push bc
-	push de
-	push hl
-	ld a, [rOBP0]
-	ld b, a
-	ld a, [wLastOBP0]
-	cp b
-	jr z, .noChangeInOBP0
-	ld d, CONVERT_OBP0
-	farcall _UpdateGBCPal_OBP
-.noChangeInOBP0
-	pop hl
-	pop de
-	pop bc
-.notGBC
-	pop af
-	ret
-	
-UpdateGBCPal_OBP1::
-	push af
-	ld a, [hGBC]
-	and a
-	jr z, .notGBC
-	push bc
-	push de
-	push hl
-	ld a, [rOBP1]
-	ld b, a
-	ld a, [wLastOBP1]
-	cp b
-	jr z, .noChangeInOBP1
-	ld d, CONVERT_OBP1
-	farcall _UpdateGBCPal_OBP
-.noChangeInOBP1
-	pop hl
-	pop de
-	pop bc
-.notGBC
-	pop af
-	ret
-
 GBPalNormal::
 ; Reset BGP and OBP0.
 	ld a, %11100100 ; 3210
@@ -130,12 +61,3 @@ GetHealthBarColor::
 .gotColor
 	ld [hl], d
 	ret
-
-Func_3082:: ; added from pokeyellow - update audio so it doesn't "lag"
-	ldh a, [hLoadedROMBank]
-	push af
-	farcall FadeOutAudio
-	callbs Music_DoLowHealthAlarm
-	callbs Audio1_UpdateMusic
-	pop af
-	jp BankswitchCommon
