56c8622e173abf649ae1a79ddc226e5faafd371e
CreamElDudJafar <jahern2398@gmail.com>
2026-04-07 07:43:34 -0500

    Fix Substitute Can Make You Faint At 0HP
    Credit Vortiene

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -4300,7 +4300,7 @@ IgnoredOrdersText:
 GetDamageVarsForPlayerAttack:
 	xor a
 	ld hl, wDamage ; damage to eventually inflict, initialise to zero
-	ldi [hl], a
+	ld [hli], a
 	ld [hl], a
 	ld hl, wPlayerMovePower
 	ld a, [hli]
@@ -4607,8 +4607,8 @@ CalculateDamage:
 
 	xor a
 	ld hl, hDividend
-	ldi [hl], a
-	ldi [hl], a
+	ld [hli], a
+	ld [hli], a
 	ld [hl], a
 
 ; Multiply level by 2
@@ -4621,7 +4621,7 @@ CalculateDamage:
 	pop af
 .nc
 	inc hl
-	ldi [hl], a
+	ld [hli], a
 
 ; Divide by 5
 	ld a, 5
