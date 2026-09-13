a3299f8f110de427ec92fec563e3bf6de005c497
CreamElDudJafar <jahern2398@gmail.com>
2026-09-13 08:28:10 -0500

    Add Wild/Surf/Rod Pokemon in Town Map
    Push A on locations in town map. Adapted/credited from Vortiene/PureRGB.

--- ram/wram.asm
+++ ram/wram.asm
@@ -768,8 +768,14 @@ wAreaUnknownCountdown:: db
 
 NEXTU
 wWhichTownMapLocation:: db
+wCurTownMapMaxWildDataFloorIndex::
 wFlyAnimCounter:: db
+wCurTownMapWildDataFloorIndex::
 wFlyAnimBirdSpriteImageIndex:: db
+wCurTownMapWildDataMap:: db
+; 0 = grass / ground, 1 = water, 2 = super rod, 3 = good rod, 4 = old rod
+wCurTownMapWildDataType:: db
+wCurTownMapInternalWildDataMap:: db
 
 NEXTU
 	ds 1
@@ -1153,7 +1159,10 @@ wPartyHPBarAttributes::
 	ds 20
 
 NEXTU
-	ds 29
+	ds 19
+wTownMapWildDataPrintValue:: db
+wTownMapRodCount:: db
+wTownMapRodMons:: ds 8
 ; storage buffer for various strings
 wStringBuffer:: ds 20
 
